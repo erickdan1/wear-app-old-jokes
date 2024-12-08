bfc2700c962f4a4c1da1022aeca39c316b33dbda
Erick Daniel <erickdanielalveslima@gmail.com>
2024-12-08 14:51:16 -0300

    refactor: adjustment in the code to remove warnings

--- app/src/main/java/com/example/someoldjokes/presentation/MainActivity.kt
+++ app/src/main/java/com/example/someoldjokes/presentation/MainActivity.kt
@@ -1,9 +1,3 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
- * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
- * changes to the libraries and their usages.
- */
-
 package com.example.someoldjokes.presentation
 
 import android.annotation.SuppressLint
@@ -11,24 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
-import androidx.wear.tooling.preview.devices.WearDevices
 import com.example.someoldjokes.R
-import com.example.someoldjokes.presentation.theme.SomeOldJokesTheme
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.example.someoldjokes.presentation.controller.JokeController
@@ -39,7 +17,7 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
 
-    val BASE_URL: String = "https://icanhazdadjoke.com"
+    private val baseUrl: String = "https://icanhazdadjoke.com"
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,12 +32,12 @@ class MainActivity : ComponentActivity() {
 
         // Build a Retrofit instance
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL) // Set the base URL for all API calls
+            .baseUrl(baseUrl) // Set the base URL for all API calls
             .addConverterFactory(GsonConverterFactory.create()) // Add a converter for JSON responses
             .build()
 
         // Initialize views
-        var modifiedTextView = findViewById<TextView>(R.id.jokeTextView)
+        val modifiedTextView = findViewById<TextView>(R.id.jokeTextView)
 
         // Create an instance of the JokeController interface
         val jokeController = retrofit.create(JokeController::class.java)
@@ -89,36 +67,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-/*
-@Composable
-fun WearApp(greetingName: String) {
-    SomeOldJokesTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            contentAlignment = Alignment.Center
-        ) {
-            TimeText()
-            Greeting(greetingName = greetingName)
-        }
-    }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
-}
- */
